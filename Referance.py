8b089717759d26af797ee7ca0038279a261a8832
N00rAhmed <tronn232003@gmail.com>
2024-01-20 17:41:41 +0000

    added tests

--- Referance.py
+++ Referance.py
@@ -2,32 +2,26 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 from datetime import date
-# https://www.forbes.com/sites/christinemoorman/2018/01/12/why-apple-is-still-a-great-marketer-and-what-you-can-learn/?sh=748f802515bd
-# https://www.simplilearn.com/tutorials/marketing-case-studies-tutorial/tesla-marketing-strategy
 
-header = input('Header: ')
+def main():
+    header = input('Header: ')
+    published_year = input('Published year of web page: ')
+    url = input('URL: ')
 
-published_year = input('Published year of web page: ')
+    reqs = requests.get(url)
 
-url = input('URL: ')
-reqs = requests.get(url)
+    # Gets current date
+    today = date.today()
 
-today = date.today()
-# dd/mm/YY
-access_date = today.strftime("%d/%m/%Y")
+    # Formats current date as a string in the "day/month/year" format
+    access_date = today.strftime("%d/%m/%Y")
 
-soup = BeautifulSoup(reqs.text, 'html.parser')
+    soup = BeautifulSoup(reqs.text, 'html.parser')
 
-# instead of function create while loop or for loop to keep iterating same program
-for title in soup.find_all('title'):
-    ref =  header + f' ({published_year}) ' + (title.get_text() + ' [online]' + ' Address: ' + url + f' [accessed: {access_date}] ')
-
-    # print(ref)
-
-    file = open('Database.txt', 'a')
-    storage = file.write(str(ref) + '\n')
-
-    file.close()
+    for title in soup.find_all('title'):
+        ref = header + f' ({published_year}) ' + (title.get_text() + ' [online]' + ' Address: ' + url + f' [accessed: {access_date}] ')
+        with open('Database.txt', 'a') as file:
+            storage = file.write(str(ref) + '\n')
 
     pull_data = input('Type pull to see all your references, if not then press enter: ')
     if pull_data == 'pull':
@@ -35,5 +29,9 @@ for title in soup.find_all('title'):
             contents = f.read()
             print(contents)
     else:
-        ('Bye')
+        print('Bye')
         sys.exit()
+        
+
+if __name__ == "__main__":
+    main()
---
+++ Test_Referance.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest.mock import patch
+from Referance import main
+
+class TestReferenceProgram(unittest.TestCase):
+
+    @patch('builtins.input', side_effect=['Sample Header', '2022', 'https://www.forbes.com/sites/christinemoorman/2018/01/12/why-apple-is-still-a-great-marketer-and-what-you-can-learn/?sh=3f9c8c8315bd', 'pull'])
+    @patch('builtins.print')
+    def test_main_function(self, mock_print, mock_input):
+        main()
+        expected_reference = 'Sample Header (2022) Why Apple Is Still A Great Marketer And What You Can Learn [online] Address: https://www.forbes.com/sites/christinemoorman/2018/01/12/why-apple-is-still-a-great-marketer-and-what-you-can-learn/?sh=3f9c8c8315bd [accessed: 20/01/2024] '
+
+        # Collect the printed content by joining the first arguments of all calls made to the mock_print object
+        # Each call represents a print statement, and call[0][0] corresponds to the printed content
+        printed_content = ''.join([call[0][0] for call in mock_print.call_args_list])
+        self.assertIn(expected_reference, printed_content)
+
+if __name__ == '__main__':
+    unittest.main()
